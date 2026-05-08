e4dfef991b7380af95824d3535908d6dc42c6ae2
Rafael-2109 <rafael6250@gmail.com>
2026-05-08 05:59:12 -0300

    feat(motos_assai): nf_qpa_adapter (DanfePDFParser + match BATEU/DIVERGENTE)

--- app/motos_assai/services/parsers/__init__.py
+++ app/motos_assai/services/parsers/__init__.py
@@ -1,1 +1,5 @@
 """Parsers de PDF/Excel do módulo Motos Assaí."""
+
+from .nf_qpa_adapter import importar_nf_qpa, NfQpaParseError, NfQpaJaImportadaError
+
+__all__ = ['importar_nf_qpa', 'NfQpaParseError', 'NfQpaJaImportadaError']
