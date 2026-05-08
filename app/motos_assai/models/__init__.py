c60bbe3b72282e119fe9a773d54e7a1bced48ab1
Rafael-2109 <rafael6250@gmail.com>
2026-05-07 23:14:09 -0300

    feat(motos_assai): add AssaiModelo + AssaiModeloAlias

--- app/motos_assai/models/__init__.py
+++ app/motos_assai/models/__init__.py
@@ -1,4 +1,17 @@
 from .cd import AssaiCd
 from .loja import AssaiLoja
+from .modelo import (
+    AssaiModelo,
+    AssaiModeloAlias,
+    ALIAS_TIPO_NOME_LIVRE,
+    ALIAS_TIPO_CODIGO_QPA,
+    ALIAS_TIPO_DESCRICAO_RECIBO,
+    ALIAS_TIPOS_VALIDOS,
+)
 
-__all__ = ['AssaiCd', 'AssaiLoja']
+__all__ = [
+    'AssaiCd', 'AssaiLoja',
+    'AssaiModelo', 'AssaiModeloAlias',
+    'ALIAS_TIPO_NOME_LIVRE', 'ALIAS_TIPO_CODIGO_QPA',
+    'ALIAS_TIPO_DESCRICAO_RECIBO', 'ALIAS_TIPOS_VALIDOS',
+]
